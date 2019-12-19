40584a473c7dc3c4684a44e93de7d4bca58cad53
shinychan95 <shinychan95@gmail.com>
2019-12-20 02:52:32 +0900

    upload m_10

--- R/m_10/m_10.R
+++ R/m_10/m_10.R
@@ -11,7 +11,7 @@ NCI60
 #h##
 # set working directory
 setwd("C:\\github\\R_to_Python\\R\\m_10")
-write.csv(NCI60,file="NCI60.csv", row.names = FALSE)
+write.csv(Violations,file="Violations.csv", row.names = FALSE)
 ####
 
 Spreads <- NCI60 %>%
@@ -20,3 +20,28 @@ Spreads <- NCI60 %>%
   summarize(N = n(), spread = sd(expression)) %>%
   arrange(desc(spread)) %>%
   mutate(order = row_number())
+
+campus_sim <- function(num_sim = 1000, wait = 10) { 
+  sally <- runif(num_sim, min = 0, max = 60) 
+  joan <- runif(num_sim, min = 0, max = 60) 
+  return(sum(abs(sally - joan) <= wait) / num_sim)
+}
+
+reps <- 5000 
+params <- data.frame(num_sims = c(100, 400, 1600)) 
+
+sim_results <- params %>%
+  group_by(num_sims) %>%
+  dplyr::do(mosaic::do(reps) * campus_sim(.$num_sims)) 
+
+favstats(campus_sim ~ num_sims, data = sim_results)
+
+sally <- runif(1000, min = 0, max = 60)
+favstats(sally)
+
+campus_sim()
+
+sally <- runif(1000, min = 0, max = 60) 
+joan <- runif(1000, min = 0, max = 60) 
+table((abs(sally - joan) <= 10))["TRUE"]
+sum(abs(sally - joan) <= 10)
